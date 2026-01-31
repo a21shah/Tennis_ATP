24af15385778d874d3a37999241724356ca70907
Abhishek <abhishek21shah@gmail.com>
2026-01-31 00:52:59 -0500

    update dicts and code working for grand slams

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 import polars as pl
+import points_dict
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
+# read file
 file_2024 = 'atp_matches_2024.csv'
-
 df = pl.read_csv(file_2024, infer_schema_length=None)
-
 print(df.shape) # (3076, 49)
 
+winner_cols_rename = {'winner_id':'player_id', 'winner_name':'player_name',}# 'winner_seed':'player_seed'} 
+non_winner_cols_rename = {'loser_id':'player_id', 'loser_name':'player_name'}#, 'loser_seed':'player_seed'}
 
+keep_cols = ['tourney_name', 'surface', 'draw_size', 'tourney_level', 'tourney_date', 'winner_id', 'winner_name', 'winner_seed', 'loser_id', 'loser_name', 'loser_seed', 'round']
+df1 = df[keep_cols].clone()
+
+### Grand Slams ###
+df_gs = df1.filter(pl.col('tourney_level')=='G')
+
+df_gs_non_winner = df_gs.with_columns(pl.col("round").replace_strict(points_dict.points_GS, default=None).alias("points"))
+df_gs_non_winner = df_gs_non_winner[['tourney_name','tourney_level','loser_id','loser_name','points']].rename(non_winner_cols_rename)
+
+df_gs_winner = (
+    df_gs
+    .filter(pl.col('round') == 'F')
+    .with_columns([
+        pl.lit('W').alias('round'),
+        pl.lit(points_dict.points_GS['W']).alias('points')
+    ])
+)
+df_gs_winner = df_gs_winner[['tourney_name','tourney_level','winner_id','winner_name','points']].rename(winner_cols_rename)
+
+
+df_gs_non_winner = df_gs_non_winner.with_columns(pl.col('points').cast(pl.Int32))
+df_gs = pl.concat([df_gs_non_winner, df_gs_winner])
+# print(df_gs.head(5))
+
+df_gs = df_gs.pivot(
+    values = "points",
+    index = ["player_id", "player_name"],
+    on = ["tourney_name", "tourney_level"],
+    aggregate_function="first"
+)
+
+print(df_gs.filter(pl.col("player_name") == "Carlos Alcaraz"))
--- points_dict.py
+++ points_dict.py
@@ -1,28 +1,52 @@
-# Dictionary for points awarded in each tournament (250/500/ATP Masters/GS/ATP Finals)
+# Dictionary for points awarded in each tournament as of 2024 (250/500/Masters/GS/ATP Finals)
 # https://en.wikipedia.org/wiki/ATP_rankings#Points_distribution_(2024%E2%80%93present)
 
-points_250 = {
+# Points awarded for a 250 tournament with a 48 player draw (Top 16 seeds receive a bye to R32)
+points_250_48 = {
     'W': 250,
     'F': 165,
     'SF': 100,
     'QF': 50,
     'R16': 25,
     'R32': 13,
+    'R64': 0
 }
 
-points_500 = {
+# Points awarded for a 250 tournament with a 32 (or 28) player draw
+points_250_32 = {
+    'W': 250,
+    'F': 165,
+    'SF': 100,
+    'QF': 50,
+    'R16': 25,
+    'R32': 0,
+}
+
+# Points awarded for a 500 tournament with a 48 player draw (Top 16 seeds receive a bye to R32)
+points_500_48 = {
     'W': 500,
     'F': 330,
     'SF': 200,
     'QF': 100,
     'R16': 50,
     'R32': 25,
+    'R64': 0
+}
+
+# Points awarded for a 500 tournament with a 32 player draw
+points_500_32 = {
+    'W': 500,
+    'F': 330,
+    'SF': 200,
+    'QF': 100,
+    'R16': 50,
+    'R32': 0
 }
 
 # There are two different types of ATP Masters. 
 # 7 ATP Masters 1000 events have a 96 player draw. The top 32 players recieve a bye into the 2nd round.
 # Monte-Carlo and Paris have a 56 player draw and thus have one less round. The top 8 players recieve a bye into the 2nd round.
-points_1000 = {
+points_1000_96 = {
     'W': 1000, 
     'F': 650,
     'SF': 400,
@@ -31,6 +55,16 @@ points_1000 = {
     'R32': 50,
     'R64': 30,
     'R128': 10,
+}
+
+points_1000_56 = {
+    'W': 1000, 
+    'F': 650,
+    'SF': 400,
+    'QF': 200,
+    'R16': 100,
+    'R32': 50,
+    'R64': 10
 }
 
 points_GS = {
@@ -44,7 +78,7 @@ points_GS = {
     'R128': 10,
 }
 
-# ATP Finals a player recieves points for every Round Robin match won (RR)
+# ATP Finals: A player recieves points for every Round Robin match won (RR)
 points_ATP_Finals = {
     'W': 900,
     'F': 400,
