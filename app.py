d2abdeab66a6d715e873861f9a814e267f867c6e
Abhishek <abhishek21shah@gmail.com>
2026-02-11 02:01:29 -0500

    read from json file + sort player select box by order in df

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
+import json
 import polars as pl
 import streamlit as st
 
 @st.cache_data
 def load_atp_points(year):
     file = f'atp_points/points_{year}.csv'
+    
     return pl.read_csv(file, infer_schema_length=None)
 
 @st.cache_data
 def tournament_level_map(df):
-    file = f'matches_data/atp_matches_{year}.csv'
-    df = pl.read_csv(file, infer_schema_length=None)
-    tourney_levels = (
-        df
-        .select(['tourney_name', 'tourney_level'])
-        .unique(maintain_order=True)
-        .group_by('tourney_level', maintain_order=True)
-        .agg(pl.col('tourney_name'))
-    )
-    tourney_levels_dict = dict(zip(tourney_levels["tourney_level"].to_list(), tourney_levels["tourney_name"].to_list()))
-    return tourney_levels_dict
+    with open(f'atp_tournaments/tournaments_{year}.json', 'r') as file:
+        data = json.load(file)
+    
+    return data
 
 year = 2025
 st.set_page_config(layout='wide')
@@ -30,26 +25,21 @@ tournament_map = tournament_level_map(df)
 
 players = st.sidebar.multiselect(
     'Players',
-    options=sorted(df['player_name'].unique()),
+    options=(df['player_name'].unique(maintain_order=True)),
     default=[]
 )
 
 tournament_levels = st.sidebar.multiselect(
-    'Tournament Type',
-    options=['ATP 250', 'ATP 500', 'Masters 1000', 'Grand Slam', 'ATP Finals'],
+    'Tournament Level',
+    options=sorted(list(tournament_map.keys())),
     default=[]
 )
 
 player_col = ['player_name']
-tournament_dict = {'ATP 250':'250', 'ATP 500':'500', 'Masters 1000':'M', 'Grand Slam':'G', 'ATP Finals':'F'}
-selected_tournaments = []
 filter_tournaments = []
 
 if tournament_levels:
-    for tournament in tournament_levels:
-            selected_tournaments.append(tournament_dict[tournament])
-
-    for selected_tournament in selected_tournaments:
+    for selected_tournament in tournament_levels:
         filter_tournaments.extend(tournament_map[selected_tournament])
 
 
